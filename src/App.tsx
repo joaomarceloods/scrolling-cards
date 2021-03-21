4a2d856f0175880df72d8762bc5a3883c9ce66ab
João Marcelo Oliveira de Souza <joaomarcelo9393@gmail.com>
2021-03-21 19:12:49 -0300

    Draft 1
    Display 7 boxes. Click on any box to select it and center it.
    Bug: wrong animation if you click on any of these slots: -3, -2, 2, 3.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { range } from "lodash";
+import { useState } from "react";
+
+const BOX_SIZE_IN_PIXELS = 100;
+const NUMBER_OF_BOXES = 20;
+const ALL_BOXES = range(NUMBER_OF_BOXES); // e.g. [0, 1, 2, ..., 17, 18, 19]
+
+const WINDOW_SIZE = 7;
+const WINDOW_START = 0 - Math.floor(WINDOW_SIZE / 2);
+const WINDOW_END = WINDOW_START + WINDOW_SIZE;
+const WINDOW_SLOTS = range(WINDOW_START, WINDOW_END); // e.g. [-3, -2, -1, 0, 1, 2, 3]
 
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {WINDOW_SLOTS.map((slot, index) => {
+        const loopingIndex = (currentIndex + slot) % 20;
+        const [thisBox] = ALL_BOXES.slice(loopingIndex);
+        const translationY = `${index * BOX_SIZE_IN_PIXELS}px`;
+        return (
+          <div
+            className="box"
+            key={thisBox}
+            onClick={() => setCurrentIndex(thisBox)}
+            style={{
+              width: BOX_SIZE_IN_PIXELS,
+              height: BOX_SIZE_IN_PIXELS,
+              transform: `translateY(${translationY})`,
+            }}
+          >
+            {slot} · {thisBox}
+          </div>
+        );
+      })}
     </div>
   );
 }
